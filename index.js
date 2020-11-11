b9ddaf59563f6d9c0b6e0218e8febb0896157c16
Worna <74328940+WornaEnkidu@users.noreply.github.com>
2020-11-11 23:59:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
  
+client.commands = new Discord.Collection();
+
+const load = (dir = "./commands/") => {
+	fs.readdirSync(dir).forEach(dirs => {
+
+		const commands = fs.readdirSync(`${dir}${dirs}`).filter(files => files.endsWith(".js"));
+
+		for (const file of commands) {
+			const command = require(`${dir}${dirs}/${file}`);
+			if (client.commands.get(command.name)) return console.warn(`${warning} Two or more commands have the same name ${command.name}.`);
+			client.commands.set(command.name, command);
+		}
+	});
+}
+
+load();
+console.log(client.commands);
+
 
 client.on('ready', () => {
 
@@ -11,19 +31,31 @@ client.on('ready', () => {
 });
 
  
+client.on('message', async message => {
+	const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName);
 
-client.on('message', message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(process.env.PREFIX)) return;
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+	try {
+		if(commandName == "unban" || commandName == "ban" || commandName == "userinfo") {
+			command.execute(message, client);
+		}else if(commandName == "reconnect"){
+			command.execute(message, client, process.env.BOT_TOKEN);
+		}
+		else {
+			command.execute(message);
+		}
+		message.delete();
+	} catch (error) {
+		console.error(error);
+		message.reply('There was an error trying to execute that command!');
+	}
 });
 
  
 
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
